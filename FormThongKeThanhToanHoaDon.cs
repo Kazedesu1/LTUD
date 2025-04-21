359667ba54bec98ac9608dfbadde877d4e599427
Kazedesu1 <111736493+Kazedesu1@users.noreply.github.com>
2025-04-21 21:54:09 +0700

    Add files via upload

---
+++ FormThongKeThanhToanHoaDon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quanLyktx
+{
+    public partial class FormThongKeThanhToanHoaDon : Form
+    {
+        public FormThongKeThanhToanHoaDon()
+        {
+            InitializeComponent();
+        }
+        Ketnoi kn = new Ketnoi();
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+            DataTable dataTable = new DataTable();
+            CrystalReport6 bcp = new CrystalReport6();
+            dataTable = kn.Lay_DulieuBang("select * from XemThongKeChuaThanhToanHoaDon");
+            bcp.SetDataSource(dataTable);
+            crystalReportViewer1.ReportSource = bcp;
+        }
+    }
+}
